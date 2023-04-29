32f477388dda2184202fe2df00369fdb810c6dbd
tnear <travis.near@outlook.com>
2023-04-29 09:54:23 -0500

    open, strcpy

---
+++ open.c
@@ -0,0 +1,53 @@
+/*
+OPEN
+
+open, openat, creat - open and possibly create a file
+
+Syntax:
+#include <fcntl.h>
+int open(const char *pathname, int flags);
+*/
+
+#include <assert.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+void openExample()
+{
+    char *filename = "my_open.txt";
+    char *content = "Open data!";
+
+    // Create file and open for reading (truncate if it exists)
+    int flags = O_CREAT | O_RDWR | O_TRUNC;
+    int fd = open(filename, flags);
+
+    // Write content to file
+    int bytesWritten = write(fd, content, strlen(content));
+    assert(bytesWritten == strlen(content));
+
+    // Seek to front of file to prepare for reading
+    lseek(fd, 0, SEEK_SET);
+
+    // Read new file's data and verify contents
+    char buffer[100];
+    int bytesRead = read(fd, buffer, sizeof(buffer));
+
+    assert(bytesRead == strlen(content));
+    assert(strncmp(buffer, content, bytesRead) == 0);
+
+    // Close file when done
+    close(fd);
+
+    // Remove file from filesystem
+    remove(filename);
+}
+
+int main()
+{
+    openExample();
+
+    printf("Tests passed!\n");
+    return 0;
+}
---
+++ strcpy.c
@@ -0,0 +1,45 @@
+/*
+STRCPY
+
+strcpy, strncpy - copy a string
+
+Syntax:
+#include <string.h>
+char* strcpy(char *dest, const char *src);
+*/
+
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+void copy()
+{
+    char str[] = "My string\n";
+    // Destination must be large enough to store the copied string
+    char dest[128];
+
+    // Copy string and verify they are equal
+    strcpy(dest, str);
+    assert(strcmp(dest, str) == 0);
+}
+
+void overwrite()
+{
+    char str[] = "Overwrite test\n";
+    char dest[25] = "Hello!";
+
+    // strcpy overwrites dest (NOT append). Use strcat to append.
+    strcpy(dest, str);
+
+    // There should be no trace of "Hello!" left
+    assert(strcmp(dest, str) == 0);
+}
+
+int main()
+{
+    copy();
+    overwrite();
+
+    printf("Tests passed!\n");
+    return 0;
+}
